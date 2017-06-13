82fe356f82ed4f3162942b65bd9546a28df6ac4a
changhuawei222 <651364894@qq.com>
2017-06-13 17:01:30 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+﻿<?php
+error_reporting(0); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/> 
+<title>溜溜溜-高考成绩单生成器🌹</title>
+<link type="text/css" rel="stylesheet" href="css/frozen.css" />
+<style>
+.ui-header-positive, .ui-footer-positive {
+background-color: #f05557;
+color: #fff;
+}
+a {
+color: #f05557;
+}
+</style>
+</head>
+<body ontouchstart="">
+<?php if($_GET['name']){?>
+	<header class="ui-header ui-header-positive ui-border-b">
+	<h1>长按下方图片点选保存图片</h1>
+</header>
+<div class="wrapper"><br><br>
+	<img src="toutu.php?name=<?=$_GET['name']?>&sex=<?=$_GET['sex']?>&shen=<?=$_GET['shen']?>" width="80%" style="margin:30px 10%;"/>
+</div>
+
+
+<?php }else{ ?>
+
+<header class="ui-header ui-header-positive ui-border-b">
+	<h1>高考成绩单生成器🌹</h1>
+</header>
+<div class="wrapper"><br>
+	<img src="toutu.jpg" width="50%" style="margin:30px 25%;"/>
+	<div class="ui-form">
+    	<form action="">
+        	<div class="ui-form-item ui-border-b">
+            	<label for="#">姓名</label>
+            	<input type="text" name="name" placeholder="输入你的姓名"/>
+        	</div>
+        	<div class="ui-form-item ui-border-b">
+            	<label for="#">性别</label>
+            	<input type="text" name="sex" placeholder="输入你的性别"/>
+        	</div>
+        	<div class="ui-form-item ui-border-b">
+            	<label for="#">省份</label>
+            	<input type="text" name="shen" placeholder="输入你的省份"/>
+        	</div>
+			<div class="ui-btn-wrap">
+    			<button class="ui-btn-lg ui-btn-danger">确定</button>
+			</div>
+    	</form>
+	</div>
+<?php 
+} 
+
+
+?>
+</body>
+</html>
